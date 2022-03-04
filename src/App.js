48134aa04e5ba652203afd3b370b95b51d461027
simon polishuk <simonp@pangea-it.com>
2022-03-04 19:02:56 +0200

    Configure React app for deployment to GitHub Pages4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Geo from './Components/Geo';
+import Notification from  './Components/Notification';
+import Camera from './Components/Camera';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Geo/>
+        <Notification/>
+        <Camera/>
       </header>
     </div>
   );
